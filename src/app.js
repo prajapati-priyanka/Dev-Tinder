f0c1f9106904083b71e9778b87f1e4fd7d2ad9cf
prajapati-priyanka <contact.priyanka.prajapati@gmail.com>
2025-01-28 23:11:03 +0530

    initialise database

--- src/app.js
+++ src/app.js
@@ -1,31 +1,16 @@
-// First we require express to create a server 
+// First we require express to create a server
 const express = require("express");
 
 const app = express();
 
-const {authAdmin} = require('./middlewares/auth');
+const connectDB = require("./config/database");
 
-// app.use("/", (err,req,res,next)=>{
-//     if(err){
-//         res.status(500).send("Error Occurred: Something went wrong");
+connectDB()
+  .then(() => {
+    console.log("Database connection successfully estalished");
+    // Now to listen to request we need to define on which port request is being lsitened
+    app.listen(3000, () => {
+      console.log("listening to server successfully on port number 3000"); // This callback will only run when server is properly setup to listen
+    });
+  }).catch((err) => console.log("Database connection failed"));
 
-//     }
-// })
-
-app.get("/getUserData", (req,res)=>{
-    throw new Error("Something went wrong");
-    res.send("User Data Sent");
-})
-
-app.use("/", (err,req,res,next)=>{
-    if(err){
-        res.status(500).send("Error Occurred: Something went wrong");
-
-    }
-})
-
-
-// Now to listen to request we need to define on which port request is being lsitened
-app.listen(3000, ()=>{
-    console.log("listening to server successfully on port number 3000") // This callback will only run when server is properly setup to listen
-})
