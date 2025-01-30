818ab71c8f6601e530a10178a5ef278ee5c537fb
prajapati-priyanka <contact.priyanka.prajapati@gmail.com>
2025-01-30 23:48:52 +0530

    add post api

--- src/app.js
+++ src/app.js
@@ -4,28 +4,24 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./config/database");
-const User = require('./models/user');
-
+const User = require("./models/user");
 
 // middleware to parse request body for all the routes.
 app.use(express.json());
 
-app.post("/signup", async(req,res)=>{
+// POST API TO SEND DATA
 
-    console.log(req.body);
+app.post("/signup", async (req, res) => {
+  // Creating a new instance of the User Model
+  const newUser = new User(req.body);
 
-    // Creating a new instance of the User Model
-    const newUser = new User(req.body);
-
-    try {
-        await newUser.save();
-        res.send("User successfully added");
-    } catch (error) {
-       res.status(500).send("Error occurred: " + error.message); 
-    }
-
-  
-})
+  try {
+    await newUser.save();
+    res.send("User successfully added");
+  } catch (error) {
+    res.status(500).send("Error occurred: " + error.message);
+  }
+});
 
 connectDB()
   .then(() => {
@@ -34,5 +30,5 @@ connectDB()
     app.listen(3000, () => {
       console.log("listening to server successfully on port number 3000"); // This callback will only run when server is properly setup to listen
     });
-  }).catch((err) => console.log("Database connection failed"));
-
+  })
+  .catch((err) => console.log("Database connection failed"));
