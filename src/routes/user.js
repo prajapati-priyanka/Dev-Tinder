9e9fa014c0aa2afbda6df762e9b9d6c4f8be2b6e
prajapati-priyanka <contact.priyanka.prajapati@gmail.com>
2025-04-22 15:08:18 +0530

    Add API to see all connections of logged in user

--- src/routes/user.js
+++ src/routes/user.js
@@ -21,6 +21,32 @@ userRouter.get("/user/requests/recieved", userAuth, async(req, res) => {
   }
 });
 
+// get all the connections of the loggedIn user.
+
+userRouter.get("/user/connections", userAuth, async(req,res)=>{
+    try {
+        const loggedInUser = req.user;
+        const userConnections = await ConnectionRequest.find({
+            $or: [
+              { fromUserId:loggedInUser, status:"accepted" },
+              { toUserId: loggedInUser, status: "accepted" },
+            ],
+          }).populate("fromUserId", "firstName lastName age gender about");
+
+
+        const filteredUserConnections = userConnections.filter(row => row.fromUserId._id.toString() !== loggedInUser._id.toString());
+
+        console.log(filteredUserConnections)
+
+          if(!userConnections){
+            throw new Error("There is no Connection for this user")
+          }
+          res.json({message:"These are the connection requests", data: filteredUserConnections})
+    } catch (error) {
+        res.send("ERROR: " + error.message);
+    }
+})
+
 
 
 module.exports = userRouter;
